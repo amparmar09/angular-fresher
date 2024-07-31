65ab216c215fe7ca99d3d237a13b90176c8ff768
Probus Insurance IT <harsh.garg.sikrai@gmail.com>
2024-07-31 18:42:27 +0530

    learn about the Authguarding

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormComponent } from './form/form.component';
+import { LoginComponent } from './components/login/login.component';
 
 
 const routes: Routes = [
-
- 
+   
   {
     path: 'regForm',
     component: FormComponent
+  },
+  {path: 'login', 
+    component : LoginComponent
   },
 {
   path: 'components',
   loadChildren: () => import ('./components/components.module').then(m=> m.ComponentsModule)
 }
-
-
 
 ];
 
